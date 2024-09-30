e5df78fbe6108a5ce172c3de8c69a37cedf9b04c
MiguelPeralvo <MiguelPeralvo>
2024-09-30 15:47:07 +0000

    Initial notebooks

---
+++ scala_run/IncludedNotebook.scala
@@ -0,0 +1,6 @@
+// Databricks notebook source
+// IncludedNotebook.scala
+
+def saludo(nombre: String): String = {
+  s"Hola, $nombre!"
+}
---
+++ scala_run/MainNotebook.scala
@@ -0,0 +1,12 @@
+// Databricks notebook source
+// MAGIC %run "./IncludedNotebook.scala"
+
+// COMMAND ----------
+
+// Uso de la función definida en IncludedNotebook
+val mensaje = saludo("Databricks")
+println(mensaje)
+
+// COMMAND ----------
+
+
